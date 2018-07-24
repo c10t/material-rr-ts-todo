d46d9b406ee8b86d1b4a52a392fe88f578668d1e
c10t <c10t@fa.ke>
2018-07-25 08:45:23 +0900

    fix: Lambdas are forbidden in JSX attributes due to their rendering performance impact

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -1,18 +1,22 @@
 import * as React from "react";
 
-import Todo from "./Todo";
 import * as State from "../states/TodoState";
+import Todo from "./Todo";
 
 export interface ITodoListProps {
   todos: State.Todos;
   onTodoClick: (id: number) => void;
 }
 
+function assignHandler(on: (id: number) => void, id: number) {
+   return () => on(id)
+}
+
 const TodoList: React.SFC<ITodoListProps> = (props) => {
   const { todos, onTodoClick } = props;
   return (
     <ul>
-      {todos.map(t => <Todo key={t.id} {...t} onClick={() => onTodoClick(t.id)}/>)}
+      {todos.map(t => <Todo key={t.id} {...t} onClick={assignHandler(onTodoClick, t.id)}/>)}
     </ul>
   )
 }
